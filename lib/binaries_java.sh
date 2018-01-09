f09248af76985b740b3ee7908fc94ac0d7e7c203
Saw Xue Zheng <sawxuezheng01@gmail.com>
2018-01-10 00:36:52 +0800

    Update binaries_java.sh

--- lib/binaries_java.sh
+++ lib/binaries_java.sh
@@ -2,17 +2,17 @@ install_java() {
   local version="$1"
   local dir="$2"
   
-  local download_url="http://download.oracle.com/otn-pub/java/jdk/8u73-b02/jdk-8u73-linux-x64.tar.gz"
+  local download_url="http://download.oracle.com/otn-pub/java/jdk/9.0.1+11/jre-9.0.1_linux-x64_bin.tar.gz"
   echo "Downloading JAVA [$download_url]"
-  curl  --silent --fail --retry 5 --retry-max-time 15 -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" "$download_url" -o /tmp/java.tar.gz || (echo "Unable to download java; does it exist?" && false)
+  curl -C - -LR#OH "Cookie: oraclelicense=accept-securebackup-cookie" -k "http://download.oracle.com/otn-pub/java/jdk/9.0.1+11/jre-9.0.1_linux-x64_bin.tar.gz"
   echo "Download complete!"
 
   echo "Installing JAVA"
   mkdir /tmp/jdk
   mkdir $dir
-  tar xzf /tmp/java.tar.gz -C /tmp/jdk
+  tar xzf jre-8u144-linux-i586.tar.gz -C /tmp/jdk
   rm -rf $dir/*
-  mv /tmp/jdk/jdk1.8.0_73/* $dir
+  mv /tmp/jdk/jre-9.0.1/* $dir
   chmod +x $dir/bin
   echo "Installation complete!"	
 }
